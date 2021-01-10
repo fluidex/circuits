ea46cdd8e383b6a8a752d805b0813b7482643a3d
HAOYUatHZ <haoyu@protonmail.com>
2021-01-10 21:08:09 +0800

    minor

--- helper.ts/account.ts
+++ helper.ts/account.ts
@@ -3,3 +3,6 @@ const Account = require("@hermeznetwork/commonjs").HermezAccount;
 const prv = 1;
 const account = new Account(prv);
 console.log(account);
+
+
+// TODO: convert bjjCompressed to bits
