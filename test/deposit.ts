949db0e8c2528f02aa1b351236cf20dbd8a4a75f
HAOYUatHZ <haoyu@protonmail.com>
2021-01-11 09:08:24 +0800

    minor

--- test/deposit.ts
+++ test/deposit.ts
@@ -1,17 +1,24 @@
 import { SimpleTest, TestComponent } from './base_test';
 import { poseidon } from 'circomlib';
 import * as path from 'path';
+const Scalar = require("ffjavascript").Scalar;
+const Account = require("@hermeznetwork/commonjs").HermezAccount;
 
 class TestDepositToNew implements SimpleTest {
   getInput() {
-    let leaves = [BigInt(10), BigInt(11), BigInt(12), BigInt(13)];
-    let midLevel = [poseidon([leaves[0], leaves[1]]), poseidon([leaves[2], leaves[3]])];
-    let root = poseidon(midLevel);
-    // check leaves[2] in this tree
-    let leaf = leaves[2];
-    let path_elements = [[leaves[3]], [midLevel[0]]];
-    let path_index = [0, 1];
-    return { leaf, path_elements, path_index, root };
+    // let leaves = [BigInt(10), BigInt(11), BigInt(12), BigInt(13)];
+    // let midLevel = [poseidon([leaves[0], leaves[1]]), poseidon([leaves[2], leaves[3]])];
+    // let root = poseidon(midLevel);
+    // // check leaves[2] in this tree
+    // let leaf = leaves[2];
+    // let path_elements = [[leaves[3]], [midLevel[0]]];
+    // let path_index = [0, 1];
+    // return { leaf, path_elements, path_index, root };
+    const tokenID = 1;
+    const prvkey = 1;
+    const account = new Account(prvkey);
+
+    return { tokenID };
   }
   getOutput() {
     return {};
