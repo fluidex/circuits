cd07c6d2159a8a1cb8479c9a8a8aaa0463ed1aac
HAOYUatHZ <haoyu@protonmail.com>
2021-01-11 22:06:50 +0800

    refactor

--- test/hash_state.ts
+++ test/hash_state.ts
@@ -2,32 +2,7 @@ import * as path from 'path';
 import { poseidon } from 'circomlib';
 import { Scalar } from 'ffjavascript';
 import { SimpleTest, TestComponent } from './base_test';
-import { stateUtils } from '@hermeznetwork/commonjs';
-
-// import { Scalar } from 'ffjavascript';
-// import { stateUtils } from '@hermeznetwork/commonjs';
-
-// const state = {
-//     tokenID: 1,
-//     nonce: 49,
-//     balance: 12343256,
-//     sign: 1,
-//     ay: "144e7e10fd47e0c67a733643b760e80ed399f70e78ae97620dbb719579cd645d",
-//     ethAddr: "0x7e5f4552091a69125d5dfcb7b8c2659029395bdf".replace("0x", ""),
-// };
-
-// const input = {
-//     tokenID: Scalar.e(state.tokenID),
-//     nonce: Scalar.e(state.nonce),
-//     balance: Scalar.e(state.balance),
-//     sign: Scalar.e(state.sign),
-//     ay: Scalar.fromString(state.ay, 16),
-//     ethAddr: Scalar.fromString(state.ethAddr, 16),
-// };
-
-// const output = {
-//     out: stateUtils.hashState(state),
-// };
+import { stateUtils } from '../helper.ts';
 
 const tokenID = 1;
 const loadAmount = 500;
@@ -56,7 +31,7 @@ const balanceRoot = poseidon(BigInt(1));
 const prvkey = 1;
 const account = new Account(prvkey);
 const ethAddrNoPrefix = account.ethAddr.replace("0x", "");
-const account = {
+const account_state = {
     nonce: 49,
     sign: account.sign,
     balanceRoot: balanceRoot,
@@ -64,26 +39,27 @@ const account = {
     ethAddr: ethAddrNoPrefix,
 };
 class TestHashAccount implements SimpleTest {
-//   getInput() {
-//     return {
-//       nonce: Scalar.e(state.nonce),
-//       sign: Scalar.e(state.sign),
-//       ay: Scalar.fromString(state.ay, 16),
-//       ethAddr: Scalar.fromString(state.ethAddr, 16),
-//     };
-//   }
-//   getOutput() {
-//     const output = {
-//         out: poseidon(tokenID, balance),
-//     };
-//     return output;
-//   }
-//   getComponent(): TestComponent {
-//     return {
-//       src: path.join(__dirname, '..', 'src', 'hash_state.circom'),
-//       main: 'HashAccount()',
-//     };
-//   }
+  getInput() {
+    return {
+      nonce: Scalar.e(account_state.nonce),
+      sign: Scalar.e(account_state.sign),
+      balanceRoot: Scalar.e(account_state.balanceRoot),
+      ay: Scalar.fromString(account_state.ay, 16),
+      ethAddr: Scalar.fromString(account_state.ethAddr, 16),
+    };
+  }
+  getOutput() {
+    const output = {
+      out: stateUtils.hashState(account_state),
+    };
+    return output;
+  }
+  getComponent(): TestComponent {
+    return {
+      src: path.join(__dirname, '..', 'src', 'hash_state.circom'),
+      main: 'HashAccount()',
+    };
+  }
 }
 
 export { TestHashBalance, TestHashAccount }
