251e3d32a88fe274ecaf767d65b1ff6e612ac57a
HAOYUatHZ <haoyu@protonmail.com>
2021-01-11 12:37:50 +0800

    fix hash_state

--- helper.ts/hash_state.ts
+++ helper.ts/hash_state.ts
@@ -4,7 +4,7 @@ import { stateUtils } from '@hermeznetwork/commonjs';
 const state = {
     tokenID: 1,
     nonce: 49,
-    balance: Scalar.e(12343256),
+    balance: 12343256,
     sign: 1,
     ay: "144e7e10fd47e0c67a733643b760e80ed399f70e78ae97620dbb719579cd645d",
     ethAddr: "0x7e5f4552091a69125d5dfcb7b8c2659029395bdf".replace("0x", ""),
@@ -14,7 +14,7 @@ const input = {
     tokenID: Scalar.e(state.tokenID),
     nonce: Scalar.e(state.nonce),
     balance: Scalar.e(state.balance),
-    sign: Scalar.e(state.tokenID),
+    sign: Scalar.e(state.sign),
     ay: Scalar.fromString(state.ay, 16),
     ethAddr: Scalar.fromString(state.ethAddr, 16),
 };
