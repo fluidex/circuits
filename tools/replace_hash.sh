6b0b8e0386480b2e45e5bb330a9fa2082f95f38a
HAOYUatHZ <haoyu@protonmail.com>
2021-01-27 13:08:39 +0800

    fix compatibility

--- tools/replace_hash.sh
+++ tools/replace_hash.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 set -uex
+
+SED_BIN=sed
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	SED_BIN=gsed
+fi
+
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 function use_rescue() {
-    find ../src -type f -name '*.circom' | grep -vi poseidon | xargs gsed -i 's#poseidon#rescue#g; s#Poseidon#Rescue#g;' 
-    gsed -i 's#poseidon as hash#rescue as hash#g' ../helper.ts/hash.ts
+    find ../src -type f -name '*.circom' | grep -vi poseidon | xargs $SED_BIN -i 's#poseidon#rescue#g; s#Poseidon#Rescue#g;' 
+    $SED_BIN -i 's#poseidon as hash#rescue as hash#g' ../helper.ts/hash.ts
 }
 function use_poseidon() {
-    find ../src -type f -name '*.circom' | grep -vi rescue | xargs gsed -i 's#rescue#poseidon#g; s#Rescue#Poseidon#g;' 
-    gsed -i 's#rescue as hash#poseidon as hash#g' ../helper.ts/hash.ts
+    find ../src -type f -name '*.circom' | grep -vi rescue | xargs $SED_BIN -i 's#rescue#poseidon#g; s#Rescue#Poseidon#g;' 
+    $SED_BIN -i 's#rescue as hash#poseidon as hash#g' ../helper.ts/hash.ts
 }
+
 use_rescue
 #use_poseidon
