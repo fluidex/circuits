e81125f283820eb9f17a2019d2aef0626fc986f7
张卓 <mycinbrin@gmail.com>
2021-01-27 15:32:05 +0800

    feat: add simple benchmark tool script

---
+++ tools/benchmark/export_circuit.ts
@@ -0,0 +1,14 @@
+import * as fs from "fs";
+import { TestTransfer } from "../../test/transfer"
+const { unstringifyBigInt, stringifyBigInts } = require("ffjavascript").utils;
+
+function exportCircuit(testClass, locDir) {
+	const input = testClass.getInput();
+	fs.writeFileSync(locDir + '/input.json', JSON.stringify(stringifyBigInts(input), null, 2));
+	const { src, main } = testClass.getComponent();
+	const circuitSrc = `include "${src}";
+	    component main = ${main};`
+	fs.writeFileSync(locDir + '/circuit.circom', circuitSrc);
+}
+
+exportCircuit((new TestTransfer()), 'data/transfer')
