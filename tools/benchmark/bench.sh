27afa69ae1a9baaafe1d59a6ecdea7b371cf3591
张卓 <mycinbrin@gmail.com>
2021-01-27 16:10:18 +0800

    bench: output more readable

--- tools/benchmark/bench.sh
+++ tools/benchmark/bench.sh
@@ -19,7 +19,7 @@ function bench_groth16_zkutil() {
     echo benchmark groth16 with zkutil
     pushd $CIRCUIT_DIR
     zkutil setup
-    time zkutil prove
+    (time zkutil prove) 2>zkutil.time
     zkutil verify
     popd
 }
@@ -27,19 +27,22 @@ function bench_groth16_zkutil() {
 
 function bench_plonk_plonkit() {
     echo benchmark plonkit with plonkit
-    KEY=`pwd`/keys/plonk/2pow20.key
+    CIRCUIT_POW=20
+    KEY=`pwd`/keys/plonk/2pow${CIRCUIT_POW}.key
     if [ ! -f $KEY ]; then
         mkdir -p keys/plonk
-        plonkit setup --power 20 --srs_monomial_form $KEY
+        plonkit setup --power ${CIRCUIT_POW} --srs_monomial_form $KEY
     fi
     pushd $CIRCUIT_DIR
     rm vk.bin || true # TODO: add overwrite option to plonkit
     plonkit export-verification-key --srs_monomial_form $KEY --circuit circuit.r1cs.json --vk vk.bin
-    time plonkit prove --srs_monomial_form $KEY --circuit circuit.r1cs.json --witness witness.json --proof proof.bin
+    (time plonkit prove --srs_monomial_form $KEY --circuit circuit.r1cs.json --witness witness.json --proof proof.bin) 2>plonkit.time
     plonkit verify --proof proof.bin --verification_key vk.bin
+    popd
 }
 
 #prepare_data
 bench_groth16_zkutil
-#bench_plonk_plonkit
-
+bench_plonk_plonkit
+echo -e "\n\n =========== benchmark results: ================= \n"
+head $CIRCUIT_DIR/*time
