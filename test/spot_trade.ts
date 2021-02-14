09be5b366f1dc5438bd1821cb7e815d5184e138f
HAOYUatHZ <haoyu@protonmail.com>
2021-02-14 11:39:32 +0800

    fix tests

--- test/spot_trade.ts
+++ test/spot_trade.ts
@@ -104,8 +104,18 @@ function initTestCase() {
   for (let i = 0; i < 2**accountLevels; i++) accountLeaves.push(44n + BigInt(i));
   accountLeaves[accountID1] = oldAccount1Hash;
   accountLeaves[accountID2] = oldAccount2Hash;
+  let oldAccount1Proof = getBTreeProof(accountLeaves, accountID1);
 
   /// new
+  account1BalanceLeaves[tokenID_1to2] -= amount_1to2;
+  let tmpAccount1BalanceProof = getBTreeProof(account1BalanceLeaves, tokenID_2to1);
+  account1BalanceLeaves[tokenID_2to1] += amount_2to1;
+  let newAccount1BalanceProof = getBTreeProof(account1BalanceLeaves, tokenID_2to1);
+
+  account2BalanceLeaves[tokenID_2to1] -= amount_2to1;
+  let tmpAccount2BalanceProof = getBTreeProof(account2BalanceLeaves, tokenID_1to2);
+  account2BalanceLeaves[tokenID_1to2] += amount_1to2;
+  let newAccount2BalanceProof = getBTreeProof(account2BalanceLeaves, tokenID_1to2);
 
   let newOrder1 = oldOrder1;
   newOrder1.filled_sell += amount_1to2;
@@ -120,6 +130,19 @@ function initTestCase() {
   const newOrder2Hash = hashOrderState(newOrder2);
   account2Orders[order2_id] = newOrder2Hash;
   let newOrder2Proof = getBTreeProof(account2Orders, order2_id);
+
+  let newAccount1 = oldAccount1;
+  newAccount1.balanceRoot = newAccount1BalanceProof.root;
+  newAccount1.orderRoot = newOrder1Proof.root;
+  const newAccount1Hash = hashAccountState(newAccount1);
+  let newAccount2 = oldAccount2;
+  newAccount2.balanceRoot = newAccount2BalanceProof.root;
+  newAccount2.orderRoot = newOrder2Proof.root;
+  const newAccount2Hash = hashAccountState(newAccount2);
+  accountLeaves[accountID1] = newAccount1Hash;
+  let tmpAccount2Proof = getBTreeProof(accountLeaves, accountID2);
+  accountLeaves[accountID2] = newAccount2Hash;
+  let newAccount2Proof = getBTreeProof(accountLeaves, accountID2);
 
   return {
     enabled: 1,
@@ -159,13 +182,14 @@ function initTestCase() {
     order1_token_buy_balance: account1_balance_buy,
     order2_token_sell_balance: account2_balance_sell,
     order2_token_buy_balance: account2_balance_buy,
-
+    old_account_root: oldAccount1Proof.root,
+    new_account_root: newAccount2Proof.root,
     old_account1_balance_path_elements: oldAccount1BalanceProof.path_elements,
-    // signal input tmp_account1_balance_path_elements[balanceLevels][1];
-    // signal input old_account1_path_elements[accountLevels][1];
+    tmp_account1_balance_path_elements: tmpAccount1BalanceProof.path_elements,
+    old_account1_path_elements: oldAccount1Proof.path_elements,
     old_account2_balance_path_elements: oldAccount2BalanceProof.path_elements,
-    // signal input tmp_account2_balance_path_elements[balanceLevels][1];
-    // signal input tmp_account2_path_elements[accountLevels][1];
+    tmp_account2_balance_path_elements: tmpAccount2BalanceProof.path_elements,
+    tmp_account2_path_elements: tmpAccount2Proof.path_elements,
   };
 }
 
@@ -173,7 +197,51 @@ let test_case = initTestCase();
 class TestSpotTrade implements SimpleTest {
   getInput() {
     return {
-      enabled: test_case.enabled,    
+      enabled: test_case.enabled,
+      order1_id: test_case.order1_id,
+      order1_tokensell: test_case.order1_tokensell,
+      order1_amountsell: test_case.order1_amountsell,
+      order1_tokenbuy: test_case.order1_tokenbuy,
+      order1_amountbuy: test_case.order1_amountbuy,
+      order2_id: test_case.order2_id,
+      order2_tokensell: test_case.order2_tokensell,
+      order2_amountsell: test_case.order2_amountsell,
+      order2_tokenbuy: test_case.order2_tokenbuy,
+      order2_amountbuy: test_case.order2_amountbuy,
+      amount_2to1: test_case.amount_2to1,
+      amount_1to2: test_case.amount_1to2,
+      order1_filledsell: test_case.order1_filledsell,
+      order1_filledbuy: test_case.order1_filledbuy,
+      order2_filledsell: test_case.order2_filledsell,
+      order2_filledbuy: test_case.order2_filledbuy,
+      order1_path_elements: test_case.order1_path_elements,
+      old_order1_root: test_case.old_order1_root,
+      new_order1_root: test_case.new_order1_root,
+      order2_path_elements: test_case.order2_path_elements,
+      old_order2_root: test_case.old_order2_root,
+      new_order2_root: test_case.new_order2_root,
+      order1_accountID: test_case.order1_accountID,
+      order2_accountID: test_case.order2_accountID,
+      order1_account_nonce: test_case.order1_account_nonce,
+      order2_account_nonce: test_case.order2_account_nonce,
+      order1_account_sign: test_case.order1_account_sign,
+      order2_account_sign: test_case.order2_account_sign,
+      order1_account_ay: test_case.order1_account_ay,
+      order2_account_ay: test_case.order2_account_ay,
+      order1_account_ethAddr: test_case.order1_account_ethAddr,
+      order2_account_ethAddr: test_case.order2_account_ethAddr,
+      order1_token_sell_balance: test_case.order1_token_sell_balance,
+      order1_token_buy_balance: test_case.order1_token_buy_balance,
+      order2_token_sell_balance: test_case.order2_token_sell_balance,
+      order2_token_buy_balance: test_case.order2_token_buy_balance,
+      old_account_root: test_case.old_account_root,
+      new_account_root: test_case.new_account_root,
+      old_account1_balance_path_elements: test_case.old_account1_balance_path_elements,
+      tmp_account1_balance_path_elements: test_case.tmp_account1_balance_path_elements,
+      old_account1_path_elements: test_case.old_account1_path_elements,
+      old_account2_balance_path_elements: test_case.old_account2_balance_path_elements,
+      tmp_account2_balance_path_elements: test_case.tmp_account2_balance_path_elements,
+      tmp_account2_path_elements: test_case.tmp_account2_path_elements,
     };
   }
   getOutput() {
