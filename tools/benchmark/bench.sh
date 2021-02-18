a5cd09120cdf36b9456dd512e6706e666cd60b2a
HAOYUatHZ <haoyu@protonmail.com>
2021-02-18 12:11:40 +0800

    add plonkit_lagrange

--- tools/benchmark/bench.sh
+++ tools/benchmark/bench.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -uex
 
-
+ZKUTIL_BIN=zkutil
+RAPIDSNARK_BIN=rapidsnark
 PLONKIT_BIN=plonkit
-CIRCUIT=block
+CIRCUIT=transfer
 export CIRCUIT_DIR=data/$CIRCUIT
+
+function prepare_tools() {
+    echo install zkutil
+    cargo install --git https://github.com/poma/zkutil
+    echo install rapidsnark
+    echo install plonkit
+    cargo install --git https://github.com/Fluidex/plonkit
+}
 
 function prepare_data() {
     echo process circuit in $CIRCUIT_DIR
@@ -14,24 +23,26 @@ function prepare_data() {
 function bench_groth16_snarkjs_wasm() {
     echo benchmark groth16 with snarkjs wasm
 }
+
 function bench_groth16_snarkjs_cpp() {
     echo benchmark groth16 with snarkjs cpp
     # https://github.com/iden3/rapidsnark
 }
+
 function bench_groth16_zkutil() {
     echo benchmark groth16 with zkutil
     pushd $CIRCUIT_DIR
-    zkutil setup
-    (time zkutil prove) 2>zkutil.time
-    zkutil verify
+    $ZKUTIL_BIN setup
+    (time $ZKUTIL_BIN prove) 2>zkutil.time
+    $ZKUTIL_BIN verify
     popd
 }
-
 
 function bench_plonk_plonkit() {
     echo benchmark plonkit with plonkit
     CIRCUIT_POW=20
     KEY=`pwd`/keys/plonk/2pow${CIRCUIT_POW}.key
+    KEY_LAG=`pwd`/keys/plonk/2pow${CIRCUIT_POW}_lagrange.key
     if [ ! -f $KEY ]; then
         mkdir -p keys/plonk
         $PLONKIT_BIN setup --power ${CIRCUIT_POW} --srs_monomial_form $KEY
@@ -40,7 +51,9 @@ function bench_plonk_plonkit() {
     rm vk.bin || true # TODO: add overwrite option to $PLONKIT_BIN
     $PLONKIT_BIN analyse
     $PLONKIT_BIN export-verification-key --srs_monomial_form $KEY --circuit circuit.r1cs.json --vk vk.bin
+    $PLONKIT_BIN dump-lagrange -m $KEY -l $KEY_LAG -c circuit.r1cs.json
     (time $PLONKIT_BIN prove --srs_monomial_form $KEY --circuit circuit.r1cs.json --witness witness.json --proof proof.bin) 2>plonkit.time
+    (time $PLONKIT_BIN prove -m $KEY -l $KEY_LAG -c circuit.r1cs.json -w witness.json -p proof.bin) 2>plonkit_lagrange.time
     $PLONKIT_BIN verify --proof proof.bin --verification_key vk.bin
     popd
     node profile_circuit.js $CIRCUIT_DIR
@@ -48,6 +61,7 @@ function bench_plonk_plonkit() {
 
 mkdir -p $CIRCUIT_DIR
 npx ts-node export_circuit.ts $CIRCUIT_DIR
+prepare_tools
 prepare_data
 bench_groth16_zkutil
 bench_plonk_plonkit
