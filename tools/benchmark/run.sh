af6aafaa1c4930835fe5363c67983b9d64ee0227
HAOYUatHZ <haoyu@protonmail.com>
2021-02-19 10:50:00 +0800

    fix

--- tools/benchmark/run.sh
+++ tools/benchmark/run.sh
@@ -3,14 +3,14 @@ set -uex
 
 export CIRCUIT=transfer
 
-# export HASH=poseidon
-# pushd ..
-# source ./replace_hash.sh
-# popd
-# source ./bench.sh
+export HASH=poseidon
+pushd ..
+source ./replace_hash.sh
+popd
+source ./bench_with_hash.sh
 
 export HASH=rescue
 pushd ..
 source ./replace_hash.sh
 popd
-source ./bench.sh
+source ./bench_with_hash.sh
