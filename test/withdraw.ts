0b763846317515f9986c7850fd05c7f637dd81ba
HAOYUatHZ <37070449+HAOYUatHZ@users.noreply.github.com>
2021-02-24 21:18:24 +0800

    refactor: refactor withdrawal test (#64)

--- test/withdraw.ts
+++ test/withdraw.ts
@@ -4,7 +4,7 @@ const Scalar = require('ffjavascript').Scalar;
 import { Account } from '../helper.ts/account';
 import { hashAccountState, getGenesisOrderRoot } from '../helper.ts/state-utils';
 import { SimpleTest, TestComponent } from './interface';
-import { TxType, getBTreeProof } from './common';
+import * as common from './common';
 
 // circuit-level definitions
 const balanceLevels = 2;
@@ -13,78 +13,62 @@ const accountLevels = 2;
 const genesisOrderRoot = getGenesisOrderRoot();
 
 function initTestCase() {
-  // input-level assignments and pre-processings
-  const tokenID = 2;
+  let state = new common.GlobalState(balanceLevels, accountLevels);
+
+  const tokenID = 2n;
   const amount = 300n;
+  // set sufficient balance to withdraw
+  const balance = amount + 1n;
+  const nonce = 99n;
 
-  const accountID = 1;
-  const account = new Account(1);
+  const account = new Account(2);
   const ethAddrNoPrefix = account.ethAddr.replace('0x', '');
-  const nonce = 51;
-  const balance = 500n;
+  let accountID = state.createNewAccount();
 
-  // account state
-  let balanceLeaves = [];
-  for (let i = 0; i < 2 ** balanceLevels; i++) balanceLeaves.push(10n + BigInt(i));
-  // TODO: check index bounds
-  balanceLeaves[tokenID] = balance;
-  let oldBalanceProof = getBTreeProof(balanceLeaves, tokenID);
-  balanceLeaves[tokenID] = balance - amount;
-  let newBalanceProof = getBTreeProof(balanceLeaves, tokenID);
-  const oldAccount = {
-    nonce: nonce,
-    sign: account.sign,
-    balanceRoot: oldBalanceProof.root,
-    ay: account.ay,
-    ethAddr: ethAddrNoPrefix,
-    orderRoot: genesisOrderRoot,
+  // set up account initial state
+  state.setAccountKey(accountID, account.publicKey);
+  for (let i = 0; i < 2 ** balanceLevels; i++) {
+    if (BigInt(i) == tokenID) {
+      state.setTokenBalance(accountID, tokenID, balance);
+    } else {
+      state.setTokenBalance(accountID, BigInt(i), 10n + BigInt(i));
+    }
+  }
+  state.setAccountNonce(accountID, nonce);
+  state.setAccountOrderRoot(accountID, genesisOrderRoot);
+
+  let withdrawTx = {
+    accountID: BigInt(accountID),
+    amount: amount,
+    tokenID: BigInt(tokenID),
+    signature: null,
   };
-  const oldAccountHash = hashAccountState(oldAccount);
-  const newAccount = {
-    nonce: nonce + 1,
-    sign: account.sign,
-    balanceRoot: newBalanceProof.root,
-    ay: account.ay,
-    ethAddr: ethAddrNoPrefix,
-    orderRoot: genesisOrderRoot,
-  };
-  const newAccountHash = hashAccountState(newAccount);
+  let fullWithdrawTx = state.fillWithdrawTx(withdrawTx);
+  let txhash = common.hashWithdraw(fullWithdrawTx);
+  withdrawTx.signature = common.accountSign(account, txhash);
+  state.Withdraw(withdrawTx);
 
-  // account tree
-  let accountLeaves = [];
-  for (let i = 0; i < 2 ** accountLevels; i++) accountLeaves.push(70n + BigInt(i));
-  // TODO: check index bounds
-  accountLeaves[accountID] = oldAccountHash;
-  let oldAccountProof = getBTreeProof(accountLeaves, accountID);
-  accountLeaves[accountID] = newAccountHash;
-  let newAccountProof = getBTreeProof(accountLeaves, accountID);
-
-  // TODO: construct tx and compute hash
-  let mockTxHash = hash([TxType.Withdraw, tokenID, amount]);
-  mockTxHash = hash([mockTxHash, accountID, nonce, balance]);
-  let signature = account.signHash(mockTxHash);
-
+  let block = state.forge();
+  // TODO: assert length
   return {
     enabled: 1,
-    accountID: accountID,
+    accountID: BigInt(accountID),
     amount: amount,
     tokenID: tokenID,
     nonce: nonce,
-    sigL2Hash: mockTxHash,
-    s: signature.S,
-    r8x: signature.R8[0],
-    r8y: signature.R8[1],
+    sigL2Hash: txhash,
+    s: withdrawTx.signature.S,
+    r8x: withdrawTx.signature.R8x,
+    r8y: withdrawTx.signature.R8y,
     sign: account.sign,
     balance: balance,
     ay: Scalar.fromString(account.ay, 16),
     ethAddr: Scalar.fromString(ethAddrNoPrefix, 16),
-    orderRoot: oldAccount.orderRoot,
-    balance_path_elements: oldBalanceProof.path_elements,
-    account_path_elements: oldAccountProof.path_elements,
-    oldBalanceRoot: oldBalanceProof.root,
-    newBalanceRoot: newBalanceProof.root,
-    oldAccountRoot: oldAccountProof.root,
-    newAccountRoot: newAccountProof.root,
+    orderRoot: genesisOrderRoot,
+    balance_path_elements: block.balance_path_elements[block.balance_path_elements.length-1][0],
+    account_path_elements: block.account_path_elements[block.account_path_elements.length-1][0],
+    oldAccountRoot: block.oldAccountRoots[block.oldAccountRoots.length-1],
+    newAccountRoot: block.newAccountRoots[block.newAccountRoots.length-1],
   };
 }
 
