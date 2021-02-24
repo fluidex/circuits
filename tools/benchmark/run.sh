908af157b1eb5d1ddc27e1499630236f24c7173f
HAOYUatHZ <haoyu@protonmail.com>
2021-02-24 10:44:29 +0800

    hardcode DIR

--- tools/benchmark/run.sh
+++ tools/benchmark/run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -uex
-
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export NODE_OPTIONS="--max_old_space_size=8192"
 
 # export CIRCUIT=transfer
 export CIRCUIT=block
 
 export HASH=poseidon
-pushd ..
-source ./replace_hash.sh
+pushd $DIR/..
+source $DIR/replace_hash.sh
 popd
-source ./bench_with_hash.sh
+source $DIR/bench_with_hash.sh
 
 export HASH=rescue
-pushd ..
-source ./replace_hash.sh
+pushd $DIR/..
+source $DIR/replace_hash.sh
 popd
-source ./bench_with_hash.sh
+source $DIR/bench_with_hash.sh
 
 echo -e "\n\n =========== benchmark results: ================= \n"
 for d in $(find ./ -name "*.time") ; do
