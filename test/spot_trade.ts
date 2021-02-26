b6690cfa49d647d7e5a65d021700921951966455
HAOYUatHZ <haoyu@protonmail.com>
2021-02-26 14:29:36 +0800

    good

--- test/spot_trade.ts
+++ test/spot_trade.ts
@@ -188,13 +188,6 @@ function initTestCase() {
     order1_filledbuy: oldOrder1.filled_buy,
     order2_filledsell: oldOrder2.filled_sell,
     order2_filledbuy: oldOrder2.filled_buy,
-
-    //
-    old_order1_root: oldOrder1Proof.root,
-    new_order1_root: newOrder1Proof.root,
-    old_order2_root: oldOrder2Proof.root,
-    new_order2_root: newOrder2Proof.root,
-
     order1_accountID: accountID1,
     order2_accountID: accountID2,
     order1_account_nonce: nonce1,
