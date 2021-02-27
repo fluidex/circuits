4b9bcc1595698a2911460c2d21fdcb70cd98712a
HAOYUatHZ <haoyu@protonmail.com>
2021-02-27 14:32:40 +0800

    reorg

--- test/test_all.ts
+++ test/test_all.ts
@@ -1,5 +1,5 @@
-// import { testWithInputOutput } from './c_tester';
-import { testWithInputOutput } from './wasm_tester';
+// import { testWithInputOutput } from 'tester/c';
+import { testWithInputOutput } from './tester/wasm';
 
 import { TestCheckLeafExists, TestCheckLeafExistsDisable, TestCheckLeafUpdate, TestCheckLeafUpdateDisable } from './binary_merkle_tree';
 import { TestPow5, TestInvPow5, TestRescueMimc, TestRescueHash } from './rescue';
@@ -12,20 +12,20 @@ import { TestSpotTrade } from './spot_trade';
 
 async function main() {
   try {
-    await testWithInputOutput(new TestRescueHash());
-    await testWithInputOutput(new TestCheckLeafExists());
-    await testWithInputOutput(new TestCheckLeafExistsDisable());
-    await testWithInputOutput(new TestCheckLeafUpdate());
-    await testWithInputOutput(new TestCheckLeafUpdateDisable());
-    await testWithInputOutput(new TestHashAccount());
-    await testWithInputOutput(new TestHashOrder());
+    // await testWithInputOutput(new TestRescueHash());
+    // await testWithInputOutput(new TestCheckLeafExists());
+    // await testWithInputOutput(new TestCheckLeafExistsDisable());
+    // await testWithInputOutput(new TestCheckLeafUpdate());
+    // await testWithInputOutput(new TestCheckLeafUpdateDisable());
+    // await testWithInputOutput(new TestHashAccount());
+    // await testWithInputOutput(new TestHashOrder());
     await testWithInputOutput(new TestGenesisOrderRoot());
-    await testWithInputOutput(new TestDepositToNew());
-    await testWithInputOutput(new TestDepositToOld());
-    await testWithInputOutput(new TestTransfer());
-    await testWithInputOutput(new TestWithdraw());
-    await testWithInputOutput(new TestBlock());
-    await testWithInputOutput(new TestSpotTrade());
+    // await testWithInputOutput(new TestDepositToNew());
+    // await testWithInputOutput(new TestDepositToOld());
+    // await testWithInputOutput(new TestTransfer());
+    // await testWithInputOutput(new TestWithdraw());
+    // await testWithInputOutput(new TestBlock());
+    // await testWithInputOutput(new TestSpotTrade());
   } catch (e) {
     console.error(e);
     process.exit(1);
