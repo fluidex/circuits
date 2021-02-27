258879369262ab7612e32c98acb1e2925f206d73
HAOYUatHZ <haoyu@protonmail.com>
2021-02-27 23:17:02 +0800

    add massive test

--- test/massive.ts
+++ test/massive.ts
@@ -10,7 +10,7 @@ import * as common from './common';
 const assert = require('assert').strict;
 
 // circuit-level definitions
-const nTxs = 1000;
+const nTxs = 10000n;
 const orderLevels = 20;
 const balanceLevels = 20;
 const accountLevels = 20;
@@ -23,6 +23,10 @@ function initTestCase() {
   const tokenID_1to2 = 0n;
   const tokenID_2to1 = 1n;
 
+  // amount each trade
+  const amount_1to2 = 1n;
+  const amount_2to1 = 10n;
+
   const account0 = new Account(2);
   const account1 = new Account(1);
   const account2 = new Account(0);
@@ -30,11 +34,9 @@ function initTestCase() {
   const accountID1 = state.createNewAccount();
   const accountID2 = state.createNewAccount();
 
-  /// mock existing account1 data
+  /// mock existing account1 data, ensure balance to trade
   state.setAccountKey(accountID1, account1.publicKey);
-  for (let i = 0; i < 2 ** balanceLevels; i++) {
-    state.setTokenBalance(accountID1, BigInt(i), 10n + BigInt(i));
-  }
+  state.setTokenBalance(accountID1, tokenID_1to2, amount_1to2 * nTxs * 10n);
   state.setAccountNonce(accountID1, 19n);
   // order1
   const order1_id = 1n;
@@ -44,16 +46,14 @@ function initTestCase() {
     tokensell: tokenID_1to2,
     filled_sell: 0n,
     filled_buy: 0n,
-    total_sell: 1000n,
-    total_buy: 10000n,
+    total_sell: amount_1to2 * nTxs * 100n,
+    total_buy: amount_2to1 * nTxs * 100n,
   };
   state.setAccountOrder(accountID1, order1_id, order1);
 
-  /// mock existing account2 data
+  /// mock existing account2 data, ensure balance to trade
   state.setAccountKey(accountID2, account2.publicKey);
-  for (let i = 0; i < 2 ** balanceLevels; i++) {
-    state.setTokenBalance(accountID2, BigInt(i), 20n + BigInt(i));
-  }
+  state.setTokenBalance(accountID2, tokenID_2to1, amount_2to1 * nTxs * 10n);
   state.setAccountNonce(accountID2, 29n);
   // order2
   const order2_id = 1n;
@@ -63,48 +63,34 @@ function initTestCase() {
     tokensell: tokenID_2to1,
     filled_sell: 0n,
     filled_buy: 0n,
-    total_sell: 10000n,
-    total_buy: 1000n,
+    total_sell: amount_2to1 * nTxs * 100n,
+    total_buy: amount_1to2 * nTxs * 100n,
   };
   state.setAccountOrder(accountID2, order2_id, order2);
 
   /// start txs
-  // trade amount
-  const amount_1to2 = 1n;
-  const amount_2to1 = 10n;
-  // ensure balance to trade
-  state.DepositToOld({
-    accountID: accountID1,
-    tokenID: tokenID_1to2,
-    amount: 99999999n,
-  });
-  state.DepositToOld({
-    accountID: accountID2,
-    tokenID: tokenID_2to1,
-    amount: 99999999n,
-  });
-  for (var j = 0; j < accountLevels; j++) {
-      processWithdraw[i].account_path_elements[j][0] <== account_path_elements[i][0][j][0];
+
+  for (var i = 0; i < nTxs; i++) {
+    let spotTradeTx = {
+      order1_accountID: accountID1,
+      order2_accountID: accountID2,
+      tokenID_1to2: tokenID_1to2,
+      tokenID_2to1: tokenID_2to1,
+      amount_1to2: amount_1to2,
+      amount_2to1: amount_2to1,
+      order1_id: order1_id,
+      order1_amountsell: order1.total_sell,
+      order1_amountbuy: order1.total_buy,
+      order1_filledsell: amount_1to2 * BigInt(i),
+      order1_filledbuy: amount_2to1 * BigInt(i),
+      order2_id: order2_id,
+      order2_amountsell: order2.total_sell,
+      order2_amountbuy: order2.total_buy,
+      order2_filledsell: amount_2to1 * BigInt(i),
+      order2_filledbuy: amount_1to2 * BigInt(i),
+    };
+    state.SpotTrade(spotTradeTx);
   }
-  let spotTradeTx = {
-    order1_accountID: accountID1,
-    order2_accountID: accountID2,
-    tokenID_1to2: tokenID_1to2,
-    tokenID_2to1: tokenID_2to1,
-    amount_1to2: amount_1to2,
-    amount_2to1: amount_2to1,
-    order1_id: order1_id,
-    order1_amountsell: order1.total_sell,
-    order1_amountbuy: order1.total_buy,
-    order1_filledsell: order1.filled_sell,
-    order1_filledbuy: order1.filled_buy,
-    order2_id: order2_id,
-    order2_amountsell: order2.total_sell,
-    order2_amountbuy: order2.total_buy,
-    order2_filledsell: order2.filled_sell,
-    order2_filledbuy: order2.filled_buy,
-  };
-  state.SpotTrade(spotTradeTx);
 
   let block = state.forge();
   return block;
