e58bb98c9f305c1ae5574c9004b14380ec55d580
HAOYUatHZ <haoyu@protonmail.com>
2021-02-28 14:52:29 +0800

    add darwin

--- tools/benchmark/process_circom_circuit.sh
+++ tools/benchmark/process_circom_circuit.sh
@@ -13,8 +13,11 @@ cp $DIR/../../node_modules/circom_runtime/c/*.cpp $CIRCUIT_DIR/
 cp $DIR/../../node_modules/circom_runtime/c/*.hpp $CIRCUIT_DIR/
 node $NODE_ARGS $DIR/../../node_modules/ffiasm/src/buildzqfield.js -q 21888242871839275222246405745257275088548364400416034343698204186575808495617 -n Fr
 # mv -f fr.asm fr.cpp fr.hpp $CIRCUIT_DIR/
-# if linux
-nasm -felf64 $CIRCUIT_DIR/fr.asm
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	nasm -fmacho64 --prefix _  $CIRCUIT_DIR/fr.asm
+else
+	nasm -felf64 $CIRCUIT_DIR/fr.asm
+fi
 
 node $NODE_ARGS $DIR/../../node_modules/circom/cli.js $CIRCUIT_DIR/circuit.circom -r $CIRCUIT_DIR/circuit.r1cs -c $CIRCUIT_DIR/circuit.c -s $CIRCUIT_DIR/circuit.sym -v
 # npx snarkjs r1cs export json circuit.r1cs circuit.r1cs.json
@@ -22,8 +25,11 @@ node $NODE_ARGS $DIR/../../node_modules/circom/cli.js $CIRCUIT_DIR/circuit.circo
 # generate the witness using snarkjs
 # node $NODE_ARGS $DIR/../../node_modules/snarkjs/build/cli.cjs wc circuit.wasm input.json witness.wtns
 # generate the witness using c_tester
-g++ -pthread $CIRCUIT_DIR/main.cpp $CIRCUIT_DIR/calcwit.cpp $CIRCUIT_DIR/utils.cpp $CIRCUIT_DIR/fr.cpp $CIRCUIT_DIR/fr.o $CIRCUIT_DIR/circuit.c -o $CIRCUIT_DIR/circuit -lgmp -std=c++11 -O3 -fopenmp -DSANITY_CHECK
-# if linux
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	g++ $CIRCUIT_DIR/main.cpp $CIRCUIT_DIR/calcwit.cpp $CIRCUIT_DIR/utils.cpp $CIRCUIT_DIR/fr.cpp $CIRCUIT_DIR/fr.o $CIRCUIT_DIR/circuit.c -o $CIRCUIT_DIR/circuit -lgmp -std=c++11 -O3 -fopenmp -DSANITY_CHECK
+else
+	g++ -pthread $CIRCUIT_DIR/main.cpp $CIRCUIT_DIR/calcwit.cpp $CIRCUIT_DIR/utils.cpp $CIRCUIT_DIR/fr.cpp $CIRCUIT_DIR/fr.o $CIRCUIT_DIR/circuit.c -o $CIRCUIT_DIR/circuit -lgmp -std=c++11 -O3 -fopenmp -DSANITY_CHECK
+fi
 ./circuit $CIRCUIT_DIR/input.json $CIRCUIT_DIR/witness.wtns
 
 # convert the witness to json, zkutil still needs this
