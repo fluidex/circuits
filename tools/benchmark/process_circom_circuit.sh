3f1d02904cc8efd0db8ce84c8d50f7469f709091
HAOYUatHZ <haoyu@protonmail.com>
2021-02-28 10:08:38 +0800

    inor

--- tools/benchmark/process_circom_circuit.sh
+++ tools/benchmark/process_circom_circuit.sh
@@ -3,14 +3,16 @@ set -ex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# 28672 = 28*1024
+
 pushd $CIRCUIT_DIR
-node --max_old_space_size=8192 --stack-size=65500 $DIR/../../node_modules/circom/cli.js circuit.circom --r1cs --wasm --sym -v
+node --max_old_space_size=28672 --stack-size=65500 $DIR/../../node_modules/circom/cli.js circuit.circom --r1cs --wasm --sym -v
 # npx snarkjs r1cs export json circuit.r1cs circuit.r1cs.json
 
 # generate the witness using snarkjs
-node --max_old_space_size=8192 --stack-size=65500 $DIR/../../node_modules/snarkjs/build/cli.cjs wc circuit.wasm input.json witness.wtns
+node --max_old_space_size=28672 --stack-size=65500 $DIR/../../node_modules/snarkjs/build/cli.cjs wc circuit.wasm input.json witness.wtns
 
 # convert the witness to json, zkutil still needs this
-node --max_old_space_size=8192 --stack-size=65500 $DIR/../../node_modules/snarkjs/build/cli.cjs wej witness.wtns witness.json
+node --max_old_space_size=28672 --stack-size=65500 $DIR/../../node_modules/snarkjs/build/cli.cjs wej witness.wtns witness.json
 
 popd
