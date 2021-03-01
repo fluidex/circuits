e0346253963a77baa1a94d0a311359bd688e7f28
HAOYUatHZ <haoyu@protonmail.com>
2021-03-01 11:27:14 +0800

    add hardhat

--- test/massive.ts
+++ test/massive.ts
@@ -10,7 +10,7 @@ import * as common from './common';
 const assert = require('assert').strict;
 
 // circuit-level definitions
-const nTxs = 10000n;
+const nTxs = 1000n;
 const orderLevels = 20;
 const balanceLevels = 20;
 const accountLevels = 20;
