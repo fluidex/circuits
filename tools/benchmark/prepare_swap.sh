94b99efb44dffaee0e19dcb9e31b444937e5b496
HAOYUatHZ <haoyu@protonmail.com>
2021-03-01 14:54:45 +0800

    add prepare_swap.sh & compile_batched_node.sh

---
+++ tools/benchmark/prepare_swap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -uex
+
+sudo mkswap -f /dev/xvdb
+sudo swapon /dev/xvdb
+
+sudo fallocate -l 400G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+sudo sh -c 'echo "vm.max_map_count=10000000" >>/etc/sysctl.conf'
+sudo sh -c 'echo 10000000 > /proc/sys/vm/max_map_count'
