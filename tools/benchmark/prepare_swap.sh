a517a58533a830c280becae2d7d0a1366eb48842
HAOYUatHZ <haoyu@protonmail.com>
2021-03-01 16:39:56 +0800

    minor

--- tools/benchmark/prepare_swap.sh
+++ tools/benchmark/prepare_swap.sh
@@ -4,10 +4,10 @@ set -uex
 # sudo mkswap -f /dev/xvdb
 # sudo swapon /dev/xvdb
 
-sudo fallocate -l 400G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
+# sudo fallocate -l 400G /swapfile
+# sudo chmod 600 /swapfile
+# sudo mkswap /swapfile
+# sudo swapon /swapfile
 
 sudo sh -c 'echo "vm.max_map_count=10000000" >>/etc/sysctl.conf'
 sudo sh -c 'echo 10000000 > /proc/sys/vm/max_map_count'
