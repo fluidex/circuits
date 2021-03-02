3212f69580184cce3b69abff2f655a6999a10794
HAOYUatHZ <haoyu@protonmail.com>
2021-03-02 13:44:06 +0800

    use swap

--- tools/benchmark/prepare_swap.sh
+++ tools/benchmark/prepare_swap.sh
@@ -4,14 +4,15 @@ set -uex
 # sudo mkswap -f /dev/xvdb
 # sudo swapon /dev/xvdb
 
-sudo fallocate -l 94G /swapfile
+sudo fallocate -l 19G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 
 # TODO: maybe increase 122880 by 94G?
 
-# sudo sh -c 'echo "vm.max_map_count=10000000" >>/etc/sysctl.conf'
-# sudo sh -c 'echo 10000000 > /proc/sys/vm/max_map_count'
-sudo sh -c 'echo "vm.max_map_count=122880" >>/etc/sysctl.conf'
-sudo sh -c 'echo 122880 > /proc/sys/vm/max_map_count'
+# https://stackoverflow.com/questions/38558989/node-js-heap-out-of-memory/59923848#59923848
+sudo sh -c 'echo "vm.max_map_count=10000000" >>/etc/sysctl.conf'
+sudo sh -c 'echo 10000000 > /proc/sys/vm/max_map_count'
+# sudo sh -c 'echo "vm.max_map_count=655300" >>/etc/sysctl.conf'
+# sudo sh -c 'echo 655300 > /proc/sys/vm/max_map_count'
