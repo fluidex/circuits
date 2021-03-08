8ff9a5b4cc98d3010471953bafb27f1c3a41f9e4
HAOYUatHZ <haoyu@protonmail.com>
2021-03-08 19:40:41 +0800

    minor

--- helper.ts/ec.ts
+++ helper.ts/ec.ts
@@ -1,3 +1,6 @@
+
+// https://gist.github.com/nakov/1dcbe26988e18f7a4d013b65d8803ffc
+// https://github.com/ethers-io/ethers.js/issues/447
 function recoverPublicKeyFromSignature(signature: string, msg: string) string {
 	return "";
 }
