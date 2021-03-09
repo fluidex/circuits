b3738491e526e4a38a223b6a75b4d06450be7b75
HAOYUatHZ <haoyu@protonmail.com>
2021-03-09 15:57:50 +0800

    some cleaning

--- helper.ts/account_test.ts
+++ helper.ts/account_test.ts
@@ -3,12 +3,6 @@ import * as assert from 'assert';
 const keccak256 = require('js-sha3').keccak256;
 import { get_create_l2_account_msg, recoverPublicKeyFromSignature } from './account';
 
-async function TestArrayBuffer() {
-	let a = [0x01, 0x02];
-	console.log(a);
-	console.log(Buffer.from(a));
-}
-
 async function TestRecoverPublicKeyAndAddress() {
 	const MNEMONIC = "radar blur cabbage chef fix engine embark joy scheme fiction master release";
 	const wallet = ethers.Wallet.fromMnemonic(MNEMONIC, null);
