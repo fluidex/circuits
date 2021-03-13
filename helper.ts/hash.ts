06c08f94fcabb52643ba7e8231531fbdc6b96fee
Zhang Zhuo <mycinbrin@gmail.com>
2021-03-13 17:59:50 +0800

    wasm rescue. 50s -> 13s (#107)

--- helper.ts/hash.ts
+++ helper.ts/hash.ts
@@ -1,3 +1,29 @@
 import { poseidon } from 'circomlib';
-import { rescueHash as rescue } from './rescue_hash';
+
+import { rescueHash as rescueHashJs } from './rescue_hash';
+
+function evenCharHex(n/*: number|bigint*/): string {
+  var hex = n.toString(16);
+  if (hex.length % 2 == 1) {
+    hex = "0" + hex;
+  }
+  return hex;
+}
+const rescueWasm = require('./rescue_wasm/rescue-wasm.js');
+function rescueHashBigInt(input: Uint8Array): bigint {
+  let resultBytes: Uint8Array = rescueWasm.rescueHash(input);
+  let resultBigInt =
+    '0x' +
+    Array.from(resultBytes).map(evenCharHex)
+      .join('');
+  return BigInt(resultBigInt);
+}
+function rescueHashWasm(input: any/*Array<bigint>*/): any/*bigint*/ {
+  let resultBigInt: string = rescueWasm.rescueHashHex(input.map(evenCharHex));
+  return BigInt(resultBigInt);
+}
+
+const rescue = rescueHashWasm;
+//const rescue = rescueHashJs;
+
 export { rescue as hash };
