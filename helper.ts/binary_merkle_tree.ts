bf5ca0c609dc8475e02801a9b839e20a158b9598
Zhang Zhuo <mycinbrin@gmail.com>
2021-03-16 00:35:28 +0800

    refactor: improve merkle tree and add some benchmarks (#118)
    * minor: add some benchmark
    
    * feature: merkle tree parallel pseudo code
    
    * minor

--- helper.ts/binary_merkle_tree.ts
+++ helper.ts/binary_merkle_tree.ts
@@ -19,13 +19,16 @@ class Tree<T> {
     }
     this.data = Array.from({ length: height + 1 }, () => new Map());
   }
-  print(dense = true, emptyLabel = (height, value) => 'None') {
+  maxLeafNum() {
+    return Math.pow(2, this.height);
+  }
+  print(dense = true, emptyLabel = 'None') {
     console.log(`Tree(height: ${this.height}, leafNum: ${Math.pow(2, this.height)}, nonEmptyLeafNum: ${this.data[0].size})`);
     if (dense) {
       for (let i = 0; i < this.data.length; i++) {
         process.stdout.write(i == 0 ? 'Leaves\t' : `Mid(${i})\t`);
         for (let j = 0; j < Math.pow(2, this.height - i); j++) {
-          process.stdout.write(this.data[i].has(BigInt(j)) ? this.data[i].get(BigInt(j)).toString() : emptyLabel(i, this.defaultNodes[i]));
+          process.stdout.write(this.data[i].has(BigInt(j)) ? this.data[i].get(BigInt(j)).toString() : emptyLabel);
           process.stdout.write(',');
         }
         process.stdout.write('\n');
@@ -64,25 +67,66 @@ class Tree<T> {
     this.data[level].set(idx, hash([this.getValue(level - 1, idx * 2n), this.getValue(level - 1, idx * 2n + 1n)]));
   }
   setValue(idx: bigint, value: T) {
+    if (this.getLeaf(idx) === value) {
+      return;
+    }
     this.data[0].set(idx, value);
     for (let i = 1; i <= this.height; i++) {
       idx = this.parentIdx(idx);
       this.recalculateParent(i, idx);
     }
   }
-  fillWithLeaves(leaves: Array<T> | Map<bigint, T>) {
-    if (Array.isArray(leaves)) {
-      if (leaves.length != Math.pow(2, this.height)) {
-        throw Error('invalid leaves size ' + leaves.length);
+  // of course there is no such thing 'parallel' in Js
+  // this function is only used as pseudo code for future Rust rewrite
+  setValueParallel(idx: bigint, value: T) {
+    // the precalculating can be done parallelly
+    let precalculated = new Array<Array<T>>();
+    let curIdx = idx;
+    let curValue = value;
+    for (let i = 0; i < this.height; i++) {
+      const pair = curIdx % 2n === 0n ? [curValue, this.getValue(i, curIdx + 1n)] : [this.getValue(i, curIdx - 1n), curValue];
+      curValue = hash(pair);
+      curIdx = this.parentIdx(curIdx);
+      precalculated.push([...pair, curValue]);
+    }
+    // apply the precalculated
+    let cacheMiss = false;
+    curIdx = idx;
+    curValue = value;
+    this.data[0].set(idx, value);
+    for (let i = 0; i < this.height; i++) {
+      const pair =
+        curIdx % 2n === 0n
+          ? [this.getValue(i, curIdx), this.getValue(i, curIdx + 1n)]
+          : [this.getValue(i, curIdx - 1n), this.getValue(i, curIdx)];
+      curIdx = this.parentIdx(curIdx);
+      if (!cacheMiss) {
+        if (!(precalculated[i][0] === pair[0] || precalculated[i][1] === pair[1])) {
+          // Due to this is a merkle tree, future caches will all be missed.
+          // precalculated becomes totally useless now
+          cacheMiss = true;
+          precalculated = null;
+        }
       }
-      // TODO: optimize here
-      for (let i = 0; i < leaves.length; i++) {
-        this.setValue(BigInt(i), leaves[i]);
+      if (cacheMiss) {
+        this.data[i + 1].set(curIdx, hash(pair));
+      } else {
+        this.data[i + 1].set(curIdx, precalculated[i][2]);
       }
-    } else if (leaves instanceof Map) {
-      for (let [k, v] of leaves.entries()) {
-        this.setValue(k, v);
-      }
+    }
+  }
+  fillWithLeavesArray(leaves: Array<T>) {
+    if (leaves.length != this.maxLeafNum()) {
+      throw Error('invalid leaves size ' + leaves.length);
+    }
+    // TODO: optimize here
+    for (let i = 0; i < leaves.length; i++) {
+      this.setValue(BigInt(i), leaves[i]);
+    }
+  }
+  fillWithLeavesMap(leaves: Map<bigint, T>) {
+    for (let [k, v] of leaves.entries()) {
+      this.setValue(k, v);
     }
   }
   getRoot() {
@@ -101,12 +145,26 @@ class Tree<T> {
   }
 }
 
-function getBTreeProof(leaves, index) {
-  let height = Math.round(Math.log2(leaves.length));
-  assert(Math.pow(2, height) == leaves.length, 'getBTreeProof');
-  let tree = new Tree<bigint>(height, 0n);
-  tree.fillWithLeaves(leaves);
-  return tree.getProof(index);
+function benchmarkTree() {
+  const h = 20;
+  let tree = new Tree<bigint>(h, 0n);
+  for (let i = 0; i < 100; i++) {
+    const start = Date.now();
+    const innerCount = 100;
+    for (let j = 0; j < innerCount; j++) {
+      if (j % 100 == 0) {
+        console.log(i, j);
+      }
+      tree.setValue(BigInt(j), BigInt(j + i));
+    }
+    const end = Date.now();
+    // 2021.03.15(Apple M1): 100 ops takes 4934ms. we definitely need parallel merkle tree written in Rust
+    console.log(`${innerCount} ops takes ${end - start}ms`);
+  }
 }
 
-export { Tree, getBTreeProof };
+export { Tree };
+
+if (require.main == module) {
+  benchmarkTree();
+}
