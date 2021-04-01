4f952f63aa411529c466de2f6e9f8ceeac9ceb00
Zhang Zhuo <mycinbrin@gmail.com>
2021-04-01 18:31:05 +0800

    chore: use snarkit (#138)
    * chore: use snarkit
    
    * minor

--- tools/fast_test.sh
+++ tools/fast_test.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 set -exu
 
-function setup() {
-	#npm i
-	npx tsc
-	# TODO how to handle this?
-	cp helper.ts/rescue_wasm/rescue-wasm_bg.wasm dist/helper.ts/rescue_wasm/
-	ln -s ../node_modules dist || true
-	ln -s ../src dist || true
+function checkCPU() {
+	for f in bmi2 adx
+	do
+		(cat /proc/cpuinfo |grep flags|head -n 1|grep $f) || (echo 'invalid cpu'; cat /proc/cpuinfo; exit 1)
+	done
 }
 
 function generateTestCases() {
-	time node -e 'require("./dist/test/test_all.js").exportAllTests()'
+	npx ts-node test/export_all_tests.ts
 }
 
 function testAll() {
 	for d in `ls testdata`;
 	do
-		node dist/test/cli/snarkit.js test testdata/$d &
+		npx snarkit test --backend native --witness_type bin testdata/$d &
 	done
 	for job in `jobs -p`
 	do
@@ -25,15 +23,7 @@ function testAll() {
 	done
 }
 
-function checkCPU() {
-	for f in bmi2 adx
-	do
-		(cat /proc/cpuinfo |grep flags|head -n 1|grep -q $f) || (echo 'invalid cpu'; exit 1)
-	done
-}
-
 checkCPU
-setup
 generateTestCases
 testAll
 
