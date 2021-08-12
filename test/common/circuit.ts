a66c4496c56a61327942be915f66ffd6410a3855
Zhang Zhuo <mycinbrin@gmail.com>
2021-08-12 17:15:54 +0800

    test: more rescue and poseidon tests (#179)

--- test/common/circuit.ts
+++ test/common/circuit.ts
@@ -2,6 +2,13 @@ import * as path from 'path';
 export function circuitSrcToName(s: string): string {
   return s.replace(/[ )]/g, '').replace(/[(,]/g, '_');
 }
+export function getRepoDir(): string {
+  return path.join(__dirname, '..', '..');
+}
+export function getCircomlibCircuitsDir(): string {
+  //return path.join(getRepoDir(), "node_modules/circomlib/circuits")
+  return path.join(require.resolve('circomlib'), '..', 'circuits');
+}
 export function getCircuitSrcDir(): string {
-  return path.join(__dirname, '..', '..', 'src');
+  return path.join(getRepoDir(), 'src');
 }
