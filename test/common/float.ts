dab04a65d0b55b9bf0c1f90313a6c54cd346d5aa
Weifan <weifan@blackapi.com>
2021-09-04 22:47:58 +0800

    update float.mjs to ts, add license notice and ad-hoc testing

--- test/common/float.ts
+++ test/common/float.ts
@@ -1,4 +1,7 @@
-import {Scalar} from "ffjavascript";
+/*
+    A modified version from https://github.com/hermeznetwork/commonjs: src/float40.js
+*/
+const Scalar = require('ffjavascript').Scalar;
 
 const mantisaLen = 35; 
 const exponentLen = 5;
@@ -13,9 +16,9 @@ const mantisa = Scalar.pow(2, mantisaLen) //34359738368
  * @param {Scalar} fl - Scalar encoded in float
  * @returns {Scalar} Scalar encoded in fix
  */
-export function float2Fix(fl) {
+export function decodeFloat(fl) {
     const m = (fl % mantisa);
-    const e = Math.floor(fl / mantisa);
+    const e = Math.floor(Scalar.toNumber(fl / mantisa));
 
     const exp = Scalar.pow(10, e);
 
@@ -29,7 +32,7 @@ export function float2Fix(fl) {
  * @param {String|Number} _f - Scalar encoded in fix
  * @returns {Scalar} Scalar encoded in float
  */
-export function fix2Float(_f) {
+export function encodeFloat(_f) {
     const f = Scalar.e(_f);
     if (Scalar.isZero(f)) return 0;
 
@@ -58,7 +61,7 @@ export function fix2Float(_f) {
  * @param {Scalar} fl - Scalar encoded in float
  * @returns {Scalar} Scalar encoded in fix
  */
-export function floorFix2Float(_f){
+export function roundAndEncodeFloat(_f){
     const f = Scalar.e(_f);
     if (Scalar.isZero(f)) return 0;
 
@@ -103,9 +106,29 @@ export function round(fix){
 
     const res = m + Scalar.mul(mantisa, e);
 
-    return float2Fix(res);
+    return decodeFloat(res);
 }
 
+/*
+//testing ...
+const start = Scalar.e('34360000000')
+let test1 = encodeFloat(start);
+if (test1 % mantisa != Scalar.e('3436000000'))throw new Error(`ad-hoc test fail: ${test1}`);
+let test2 = decodeFloat(test1)
+if (test2 != start)throw new Error(`ad-hoc test fail: ${test2}`);
+const startPrecious = Scalar.e('34361111111')
+try {
+    let test3 = encodeFloat(startPrecious);
+    throw `not expected passed: ${test3}`
+}catch (e) {
+    if (typeof e === 'string')throw new Error(`ad-hoc test fail: ${e}`)
+}
+let test4 = roundAndEncodeFloat(startPrecious)
+if (test4 % mantisa != Scalar.e('3436111111'))throw new Error(`ad-hoc test fail: ${test4}`);
+let test5 = round(startPrecious)
+if (Scalar.add(test5, 1) != startPrecious)throw new Error(`ad-hoc test fail: ${test5}`);
+*/
+
 export default {
-    config, float2Fix, fix2Float, floorFix2Float, round
+    config, encodeFloat, decodeFloat, roundAndEncodeFloat, round
 }
