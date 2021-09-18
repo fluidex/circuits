7bacbe9d9cf752bfe84d7de994b2322e1c1d71ff
HAOYUatHZ <haoyu@protonmail.com>
2021-09-18 22:08:10 +0800

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'prefer-const': 'off',
   },
 };
