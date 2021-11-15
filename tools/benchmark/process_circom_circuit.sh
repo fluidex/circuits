3a9c6715fc2361940053fd3831d9f8f4a5f699e2
Ubuntu <ubuntu@big.ap-northeast-1.compute.internal>
2021-11-15 09:19:30 +0000

    bench

--- tools/benchmark/process_circom_circuit.sh
+++ tools/benchmark/process_circom_circuit.sh
@@ -3,18 +3,8 @@ set -ex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-export PATH=~/node/out/Release:$PATH
-
-# heap: init 65G, max 1.4T
-export NODE_ARGS="--trace-gc --trace-gc-ignore-scavenger --max-old-space-size=1400000 --initial-old-space-size=65500 --no-global-gc-scheduling --no-incremental-marking --max-semi-space-size=1024 --initial-heap-size=65500"
-#export CIRCOM_CLI=`npm bin`/circom
-
 echo `date` start compiling circuit
-# both snarkit itself and child processes need to be run by customized node
-#SNARKIT=`which snarkit`
-SNARKIT=`npm bin`/snarkit 
-#SNARKIT=`npm bin -g`/snarkit 
-node $NODE_ARGS $SNARKIT compile $CIRCUIT_DIR --sanity_check --verbose --backend=native &> $CIRCUIT_DIR/compile.log
+npx snarkit2 compile $CIRCUIT_DIR --verbose --backend=native &> $CIRCUIT_DIR/compile.log
 echo `date` finish compiling circuit
 
-node $NODE_ARGS $SNARKIT check $CIRCUIT_DIR --sanity_check --verbose --backend=native --witness_type=bin
+npx snarkit2 check $CIRCUIT_DIR --verbose --backend=native --witness_type=bin
